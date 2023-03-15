cd0a6c7033358950172857f890a72cd1b35f890a
Juan <juan8.devmode@gmail.com>
2023-03-15 15:16:45 +0800

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+let shopItemsData = [
+    {
+        id: "sadfdsjkjhzcoivupsfsdjklg",
+        name: "Black Jacket",
+        price: 420,
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "images/img-1.jpg",
+    },
+    {
+        id: "aghdfjklancviosdfjsldkg",
+        name: "Formal Polo",
+        price: 370,
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "images/img-2.jpg",
+    },
+    {
+        id: "uiretkwerjtklwerhtjksdhf",
+        name: "Assorted Shirt",
+        price: 220,
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "images/img-3.jpg",
+    },
+    {
+        id: "poqiwejksaasdhmbxcasdf",
+        name: "Cartoon Shirt",
+        price: 100,
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "images/img-4.jpg",
+    },
+    {
+        id: "xvuyofuysgekjrjhetwkje",
+        name: "Blood Cap",
+        price: 230,
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "images/img-5.jpg",
+    },
+    {
+        id: "zajtlkrhetkjsdfga",
+        name: "Cargo Pants",
+        price: 200,
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "images/img-6.jpg",
+    },
+];
